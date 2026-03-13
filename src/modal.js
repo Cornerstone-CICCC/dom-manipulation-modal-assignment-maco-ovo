e8eb6f58efe283ea3636d48fdff59e06a50182e7
makoto2025 <aratamakoto2025@gmail.com>
2026-03-13 15:19:04 -0700

    done till No4

---
+++ src/modal.js
@@ -0,0 +1,17 @@
+// select button element
+const button = document.querySelector("button");
+const modal = document.querySelector("#modal");
+
+// add event listener on click to button
+// remove class invisible from div modal
+button.addEventListener("click", () => {
+	modal.classList.remove("invisible");
+});
+
+// add event listener on key press Escape
+// add class invisible to div modal
+document.addEventListener("keyup", (event) => {
+	if (event.key == "Escape") {
+		modal.classList.add("invisible");
+	}
+});
