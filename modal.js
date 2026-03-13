79f46c213dab0a27792c7f61b4926e05979a36d8
makoto2025 <aratamakoto2025@gmail.com>
2026-03-13 14:55:55 -0700

    Add event listener on click & key press

--- modal.js
+++
@@ -1,9 +0,0 @@
-console.log("here");
-
-// select button element
-
-// add event listener on click to button
-// remove class invisible from div modal
-
-// add event listener on key press Escape
-// add class invisible to div modal
